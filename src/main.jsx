6305a2d6482735c1c3b588eb2f309df64a09762d
Yaasien78 <141853765+Yaasien78@users.noreply.github.com>
2026-07-14 11:56:20 +0700

    Fix import statements in main.jsx
    Adjusted import statements to have single dots.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './App.jsx'  // <-- titiknya 1 aja
+import './index.css'         // <-- titiknya 1 aja
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
