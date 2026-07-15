3f18190e4610e86b767011d2479221c312bcee1b
Yaasien78 <141853765+Yaasien78@users.noreply.github.com>
2026-07-15 20:54:11 +0700

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('ai-tracker-v1').then(cache => cache.addAll(['/'])));
-  self.skipWaiting();
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
